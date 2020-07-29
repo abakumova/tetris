c96e0be6280f93bdce118dbb8071c4bf764e4ad4
Viktoriia Abakumova <abakumovaviktory@gmail.com>
2020-07-29 17:20:26 +0300

    Implement move & rotate actions

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,15 @@ const scoreElement = document.getElementById("score-value");
 let score = 0;
 let isPaused = false;
 let gameTimerID;
+let pauseButton = document.getElementById("play-pause");
 
 //0 - free; 1 - moving; 2 - fixed
 let playField = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -29,6 +30,47 @@ let playField = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
+
+let figures = {
+    0: [
+        [1, 1],
+        [1, 1],
+    ],
+    1: [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ],
+    2: [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0],
+    ],
+    3: [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0],
+    ],
+    4: [
+        [1, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+    ],
+    5: [
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0],
+    ],
+    6: [
+        [1, 1, 1],
+        [0, 1, 0],
+        [0, 0, 0],
+    ],
+};
+
+setTimeout(startGame, gameSpeed);
+draw();
 
 function draw() {
     if (!isPaused) {
@@ -49,8 +91,9 @@ function draw() {
 }
 
 function startGame() {
+    draw();
     moveDown();
-    if (!isPaused) {
+    if (!isPaused && active != null) {
         updateGameState();
         setTimeout(startGame, gameSpeed);
     }
@@ -58,109 +101,48 @@ function startGame() {
 
 function updateGameState() {
     if (!isPaused) {
-        draw();
+        if (active != null) {
+            addActive();
+            draw();
+        }
     }
 }
 
-setTimeout(startGame, gameSpeed);
-
 document.onkeydown = function (e) {
-    if (!isPaused) {
+    if (!isPaused && active != null) {
         if (e.code === "ArrowLeft") {
-            moveLeft();
+            active.x -= 1;
+            if (hasCollisions()) {
+                active.x += 1;
+            }
         } else if (e.code === "ArrowRight") {
-            moveRight();
+            active.x += 1;
+            if (hasCollisions()) {
+                active.x -= 1;
+            }
         } else if (e.code === "ArrowDown") {
             moveDown();
+        } else if (e.code === "ArrowUp") {
+            rotate();
+        } else if (e.code === "Space") {
+            drop();
         }
-        draw();
+
+        updateGameState();
     }
 };
 
-function canMoveDown() {
-    for (let y = 0; y < playField.length; y++) {
-        for (let x = 0; x < playField[y].length; x++) {
-            if (playField[y][x] === 1 && (y === playField.length - 1 || playField[y + 1][x] === 2)) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-function moveDown() {
-    if (canMoveDown()) {
-        for (let y = playField.length - 1; y >= 0; y--) {
-            for (let x = 0; x < playField[y].length; x++) {
-                if (playField[y][x] === 1) {
-                    playField[y + 1][x] = 1;
-                    playField[y][x] = 0;
-                }
-            }
-        }
+pauseButton.onmousedown = function changePlayPauseButton(e) {
+    let change = document.getElementById("play-pause");
+    if (change.className === "button pause-button") {
+        change.className = "button play-button";
+        clearTimeout(gameTimerID);
     } else {
-        fix();
+        change.className = "button pause-button";
+        gameTimerID = setTimeout(startGame, gameSpeed);
     }
-}
-
-function canMoveLeft() {
-    for (let y = 0; y < playField.length; y++) {
-        for (let x = 0; x < playField[y].length; x++) {
-            if (playField[y][x] === 1) {
-                if (x === 0 || playField[y][x - 1] === 2) {
-                    return false;
-                }
-            }
-        }
-    }
-
-    return true;
-
-}
-
-function moveLeft() {
-    if (canMoveLeft()) {
-        for (let y = playField.length - 1; y >= 0; y--) {
-            for (let x = 0; x < playField[y].length; x++) {
-                if (playField[y][x] === 1) {
-                    playField[y][x - 1] = 1;
-                    playField[y][x] = 0;
-                }
-            }
-        }
-    } else {
-        fix();
-    }
-}
-
-function canMoveRight() {
-    for (let y = 0; y < playField.length; y++) {
-        for (let x = 0; x < playField[y].length; x++) {
-            if (playField[y][x] === 1) {
-                if (x === 9 || playField[y][x + 1] === 2) {
-                    return false;
-                }
-            }
-        }
-    }
-    return true;
-}
-
-function moveRight() {
-    if (canMoveRight()) {
-        for (let y = playField.length - 1; y >= 0; y--) {
-            for (let x = playField[0].length - 1; x >= 0; x--) {
-                if (playField[y][x] === 1) {
-                    playField[y][x + 1] = 1;
-                    playField[y][x] = 0;
-                }
-            }
-        }
-    } else {
-        fix();
-    }
-}
-
+    isPaused = !isPaused;
+};
 
 function removeFullLines() {
     let canRemoveLine = true,
@@ -198,6 +180,75 @@ function removeFullLines() {
     scoreElement.innerHTML = score;
 }
 
+function getNew() {
+    const rand = Math.floor(Math.random() * 7);
+    const newTetro = figures[rand];
+
+    return {
+        x: Math.floor((10 - newTetro[0].length) / 2),
+        y: 0,
+        shape: newTetro,
+    };
+}
+
+let active = getNew();
+
+function addActive() {
+    removePreviousActive();
+    if (active != null) {
+        for (let y = 0; y < active.shape.length; y++) {
+            for (let x = 0; x < active.shape[y].length; x++) {
+                if (active.shape[y][x] === 1) {
+                    playField[active.y + y][active.x + x] =
+                        active.shape[y][x];
+                }
+            }
+        }
+    }
+}
+
+function removePreviousActive() {
+    for (let y = 0; y < playField.length; y++) {
+        for (let x = 0; x < playField[y].length; x++) {
+            if (playField[y][x] === 1) {
+                playField[y][x] = 0;
+            }
+        }
+    }
+}
+
+function rotate() {
+    if (active != null) {
+        const prevState = active.shape;
+
+        active.shape = active.shape[0].map((val, index) =>
+            active.shape.map((row) => row[index]).reverse()
+        );
+
+        if (hasCollisions()) {
+            active.shape = prevState;
+        }
+    }
+}
+
+function hasCollisions() {
+    if (active != null) {
+        for (let y = 0; y < active.shape.length; y++) {
+            for (let x = 0; x < active.shape[y].length; x++) {
+                if (
+                    active.shape[y][x] &&
+                    (playField[active.y + y] === undefined ||
+                        playField[active.y + y][active.x + x] === undefined ||
+                        playField[active.y + y][active.x + x] === 2)
+                ) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
 function fix() {
     for (let y = 0; y < playField.length; y++) {
         for (let x = 0; x < playField[y].length; x++) {
@@ -206,20 +257,32 @@ function fix() {
             }
         }
     }
-    removeFullLines();
-
-    playField[0] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0];
-    playField[1] = [0, 0, 0, 0, 1, 1, 0, 0, 0, 0];
 }
 
-function changePlayPauseButton() {
-    let change = document.getElementById("play-pause");
-    if (change.className === "button pause-button") {
-        change.className = "button play-button";
-        clearTimeout(gameTimerID);
-    } else {
-        change.className = "button pause-button";
-        gameTimerID = setTimeout(startGame, gameSpeed);
+function moveDown() {
+    if (active != null) {
+        active.y += 1;
+        if (hasCollisions()) {
+            active.y -= 1;
+            fix();
+            active = getNew();
+            removeFullLines();
+            if (hasCollisions()) {
+                active = null;
+                draw();
+            }
+        }
     }
-    isPaused = !isPaused;
+}
+
+function drop() {
+    if (active != null) {
+        for (let y = active.y; y < playField.length; y++) {
+            active.y += 1;
+            if (hasCollisions()) {
+                active.y -= 1;
+                break;
+            }
+        }
+    }
 }
